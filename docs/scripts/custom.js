6fcd9e0a60339f8d2791f6ec892c2878717a20c3
Elena <elenatokmakova551@gmail.com>
2017-07-06 10:58:59 -0400

    Fixed the layout for small and extra small screens

--- docs/scripts/custom.js
+++ docs/scripts/custom.js
@@ -19,19 +19,19 @@ $(document).ready(function() {
 
                 if (data.stream === null) {
 
-                    currentChannel = '<li class="twitchChannel" id="' + channel +'"><div class="row"><div class="col-xs-4">' + logo + '</div><div class="col-xs-4"><strong><a href="' + url + '" target="_blank">' + channel + '</a></div><div class="col-xs-4"></strong>Offline</div></div></li>';                  
+                    currentChannel = '<li class="twitchChannel" id="' + channel +'"><div class="row"><div class="col-xs-4 col-sm-4">' + logo + '</div><div class="col-xs-8 col-sm-4"><strong><a href="' + url + '" target="_blank">' + channel + '</a></div><div class="status col-sm-4"></strong>Offline</div></div></li>';                  
                     $(currentChannel).addClass("bg-danger").appendTo("#offline-ul, #all-ul");                   
                 }
                 else if (data.stream === undefined) {
 
-                     currentChannel = '<li class="twitchChannel" id="' + channel +'"><div class="row"><div class="col-xs-4"><img src="' + logo + '"></div><div class="col-xs-4"><strong><a href="' + url + '" target="_blank">' + channel + '</a></div><div class="col-xs-4"></strong>Account closed</div></div></li>';   
+                     currentChannel = '<li class="twitchChannel" id="' + channel +'"><div class="row"><div class="col-xs-4 col-sm-4"><img src="' + logo + '"></div><div class="col-xs-6 col-sm-4"><strong><a href="' + url + '" target="_blank">' + channel + '</a></div><div class="status col-sm-4"></strong>Account closed</div></div></li>';   
                     $(currentChannel).addClass("bg-danger").appendTo("#offline-ul, #all-ul"); 
                 }
                 else {
                     game = data.stream.game;                   
                     logo = data.stream.channel.logo;  
                     
-                    currentChannel = '<li class="twitchChannel" id="' + channel +'"><div class="row"><div class="col-xs-4"><img src="' + logo + '"></div><div class="col-xs-4"><strong><a href="' + url + '" target="_blank">' + channel + '</a></div><div class="col-xs-4"></strong>' + game + '</div></div></li>';  
+                    currentChannel = '<li class="twitchChannel" id="' + channel +'"><div class="row"><div class="col-xs-4 col-sm-4"><img src="' + logo + '"></div><div class="col-xs-8 col-sm-4"><strong><a href="' + url + '" target="_blank">' + channel + '</a></div><div class="status col-sm-4"></strong>' + game + '</div></div></li>';  
                     $(currentChannel).addClass("bg-success").appendTo("#online-ul, #all-ul");   
                 }
               },
